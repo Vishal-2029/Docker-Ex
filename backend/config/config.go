19f72ae4cb7d06ae7f298656f8f76696c5bc9894
Vishal_2029 <Vishal.9dot@gmail.com>
2025-02-26 17:31:31 +0530

    first commit

---
+++ backend/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func connectToDB() (*sql.DB, error) {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
